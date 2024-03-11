2154a2912fa1c66df8fd20f04c2ac7ee871a0587
Екатерина Плотникова <141966785+EkaterinaPLotnikowa@users.noreply.github.com>
2024-03-11 16:53:43 +0300

    Add files via upload

---
+++ lesson_1/lesson_1_task_7.py
@@ -0,0 +1,54 @@
+def num_0():
+    a = 0
+    return a
+
+def num_1():
+    a = 1
+    return a
+
+def num_2():
+    a = 2
+    return a
+
+def num_3():
+    a = 3
+    return a
+
+def num_4():
+    a = 4
+    return a
+
+def num_5():
+    a = 5
+    return a
+
+def num_6():
+    a = 6
+    return a
+
+def num_7():
+    a = 7
+    return a
+
+def num_8():
+    a = 8
+    return a
+
+def num_9():
+    a = 9
+    return a
+
+num_8()
+num_8()
+num_0()
+num_0()
+num_5()
+num_5()
+num_5()
+num_3()
+num_5()
+num_3()
+num_5()
+
+phone_num = f"{num_8()}{num_8()}{num_0()}{num_0()}{num_5()}{num_5()}{num_5()}{num_3()}{num_5()}{num_3()}"
+print(f"Телефон: {phone_num}")
