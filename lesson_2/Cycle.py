23b5fdc9e51d7f37fa9e6a53e11fd954116f9f3f
Плотникова Екатерина <potpotik@mail.ru>
2024-03-21 19:22:45 +0300

    Циклы

--- lesson_2/Cycle.py
+++ lesson_2/Cycle.py
@@ -1,5 +1,14 @@
-students = ["Алексей", "Наталья", "Олег", "Екатерина", "Филипп", "Виктор"]
+##students = ["Алексей", "Наталья", "Олег", "Екатерина", "Филипп", "Виктор"]
 
-for i in range(0,len(students)):
-   print(students [i])
+##for i in range(0,len(students)):
+   ##print(students [i])
 
+my_list = [1, 2, 3, 4, 5]
+ 
+for i in my_list:
+    if i == 6:
+        print("Item found!")
+        break
+    print(i)
+else:
+    print("Item not found!")
